f09e0db809ed4790d6adc8663f0c2f002afcadd7
kalash <kalash7@protonmail.com>
2019-12-14 13:15:01 +0100

    Added the CaesarCipher

--- src/CaesarCipher.py
+++ src/CaesarCipher.py
@@ -5,43 +5,66 @@ import pyperclip
 
 
 def main():
-    userInput = int()
 
-    print("CAESAR CIPHER\n----------------")
-    print('SET MODE:')
-    print('1. DECRYPT')
-    print('2. ENCRYPT')
-    userInput = int(input())
+    mode_decrypt = False  # Sets the mode, false for decrypt
+    user_input = int()
 
-    if userInput == 1:
-        print('wip')
-    elif userInput == 2:
-        encrypt()
-    else:
-        print('INVALID COMMAND, TRY AGAIN')
-        main()
+    try:
+        print("CAESAR CIPHER\n-------------")
+        print('SET MODE:')
+        print('1. DECRYPT')
+        print('2. ENCRYPT')
+
+        user_input = int(input())
+
+        if user_input == 1:
+            mode_decrypt = True
+            cipher(mode_decrypt)
+        elif user_input == 2:
+            cipher(mode_decrypt)
+        else:
+            print('INVALID COMMAND, TRY AGAIN')
+            main()
+
+    except ValueError:
+        print("ERROR, MODE SET TO ENCRYPT")
+        cipher(mode_decrypt)
 
 
-def encrypt():
+def cipher(mode):
+
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     message = ""
     encrypted = ""
-    key = 0
-    counter = 0
+    key = 0  # Added or subtracted to counter, to form the encrypted message.
+    counter = 0  # Stores the position of the letter in the alphabet
+
     message = input('ENTER A MESSAGE TO ENCRYPT: ')
-    key = int(input('SET KEY: '))
-    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     message = message.upper()
 
-    for i in message:
-        if i in alphabet:
-            test = len(alphabet)
-            counter = alphabet.find(i)
+    try:
+        key = int(input('SET KEY: '))
+    except ValueError:
+        key = 1
+        print("DEFAULT KEY: 1")
+
+    for letter in message:
+        if letter in alphabet:
+            counter = alphabet.find(letter)
+            # Wrap if the set of letters it's not enough
             if counter >= len(alphabet) - 1:
                 counter -= len(alphabet)
             elif counter < 0:
                 counter += len(alphabet)
-            encrypted += alphabet[counter + key]
+            # Encrypt or Decrypt the message
+            if mode is False:
+                encrypted += alphabet[counter + key]
+            elif mode is True:
+                encrypted += alphabet[counter - key]
+
+    print("MESSAGE COPIED TO CLIPBOARD")
     print(encrypted)
+    pyperclip.copy(encrypted)  # Copies the message to the clipboard
 
 
 if __name__ == '__main__':
